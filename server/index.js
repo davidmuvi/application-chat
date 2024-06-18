a4e9eca14eb045c7a84b620713d113e28b884805
davidmuvi <davidmunozvillalba@gmail.com>
2024-06-18 14:47:17 +0200

    nearly finished

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
 })
 
 const db = createClient({
-    url: "libsql://trusting-snake-eyes-davidmuvi.turso.io",
+    url: process.env.DB_URL,
     authToken: process.env.DB_TOKEN
 })
 
